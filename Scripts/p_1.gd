3e1106dd7f113490bd7840288281d367d1083563
nyyozu <gustavo.almeida230400@gmail.com>
2026-09-17 12:52:51 -0300

    Sprite dos players alterados, tiles alterados

---
+++ Scripts/p_1.gd
@@ -0,0 +1,37 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+@onready var sprite: AnimatedSprite3D = $AnimatedSprite3D
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	if Input.is_action_just_pressed("TAB") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir := Input.get_vector("A", "D", "W", "S")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+	_atualizar_animacao()
+
+func _atualizar_animacao() -> void:
+	var esta_andando := Vector2(velocity.x, velocity.z).length() > 0.1
+
+	if esta_andando:
+		if sprite.animation != "walk":
+			sprite.play("walk")
+		# vira o sprite pro lado que está andando
+		sprite.flip_h = velocity.x < 0
+	else:
+		if sprite.animation != "idle":
+			sprite.play("idle")
